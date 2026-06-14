1a2d47b7e31b7a053f89e0cf49609252877e69df
trCVZ <tristan.cvz.pro@gmail.com>
2026-06-14 22:51:15 +0200

    feat: add long print with more informations when -l option is enabled

--- ls.cpp
+++ ls.cpp
@@ -1,7 +1,9 @@
+#include <iomanip>
 #include <iostream>
 #include <filesystem>
 #include <string>
 #include <expected>
+#include <chrono>
 
 enum Error {
     NotADirectory,
@@ -78,6 +80,83 @@ std::expected<ParsedArguments, Error> parseArguments(const int argc, const char*
     return ParsedArguments{dir, options};
 }
 
+std::string permissionsToString(std::filesystem::perms p) {
+    std::string s;
+
+    using std::filesystem::perms;
+
+    s += (p & perms::owner_read) != perms::none ? "r" : "-";
+    s += (p & perms::owner_write) != perms::none ? "w" : "-";
+    s += (p & perms::owner_exec) != perms::none ? "x" : "-";
+
+    s += (p & perms::group_read) != perms::none ? "r" : "-";
+    s += (p & perms::group_write) != perms::none ? "w" : "-";
+    s += (p & perms::group_exec) != perms::none ? "x" : "-";
+
+    s += (p & perms::others_read) != perms::none ? "r" : "-";
+    s += (p & perms::others_write) != perms::none ? "w" : "-";
+    s += (p & perms::others_exec) != perms::none ? "x" : "-";
+
+    return s;
+}
+
+std::string fileTypeToString(const std::filesystem::file_type type) {
+    namespace fs = std::filesystem;
+
+    switch (type) {
+        case fs::file_type::none:
+            return "?";
+
+        case fs::file_type::not_found:
+            return "!";
+
+        case fs::file_type::regular:
+            return "-";
+
+        case fs::file_type::directory:
+            return "d";
+
+        case fs::file_type::symlink:
+            return "l";
+
+        case fs::file_type::block:
+            return "b";
+
+        case fs::file_type::character:
+            return "c";
+
+        case fs::file_type::fifo:
+            return "p";
+
+        case fs::file_type::socket:
+            return "s";
+
+        case fs::file_type::unknown:
+            return "u";
+
+        default:
+            return "?";
+    }
+}
+
+std::string lastWriteDate(const std::filesystem::directory_entry& entry) {
+
+    const std::chrono::system_clock::time_point systemTime = std::chrono::clock_cast<std::chrono::system_clock>(entry.last_write_time());
+
+    return std::format("{:%d/%m/%Y}", floor<std::chrono::days>(systemTime));
+}
+
+void printLong(const std::filesystem::directory_entry& entry) {
+    std::string type = fileTypeToString(entry.status().type());
+    std::string perms = permissionsToString(entry.status().permissions());
+
+    std::uintmax_t size{entry.file_size()};
+
+    std::string name = entry.path().filename().string();
+    std::string date = lastWriteDate(entry);
+
+    std::cout << perms << " " << type << " " << std::setw(12) <<size << " " << date << " " << name << "\n";
+}
 
 int main(const int argc, const char** argv) {
 
@@ -91,14 +170,18 @@ int main(const int argc, const char** argv) {
     std::filesystem::directory_entry dir{parsedArguments->dir};
     int options{parsedArguments->options};
 
-    std::string sep = (options & 1) == 1 ? "\n" : " ";
-
     for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator{dir}) {
         std::string entryString{entry.path().filename().string()};
         if (!entryString.starts_with(".") || (options & 2) == 2) {
-            std::cout << entryString << sep;
+            if ((options & 1) == 1) {
+                printLong(entry);
+            }
+            else {
+                std::cout << entryString << " ";
+            }
         }
     }
 
+    std::cout << "\n";
     return 0;
 }
